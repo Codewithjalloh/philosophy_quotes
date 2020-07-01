64186cb5105dd166bd3c4e54e680f3224df543d7
David. J <codewithjalloh@gmail.com>
2020-07-01 18:35:55 +0100

    adding comments

---
+++ android/app/src/main/kotlin/codewithjalloh/com/philosophyquotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package codewithjalloh.com.philosophyquotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:philosophyquotes/ui/quotes_master_detail.dart';
 
+// main app
 void main() {
   runApp(MyApp());
 }
 
+// stateless widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // return material app
     return MaterialApp(
+      // app title
       title: 'Philosophy Quotes',
+      // theme widget
       theme: ThemeData(
+        // color blue
         primarySwatch: Colors.blue,
       ),
+      // direct to quote screen
       home: QuotesMasterDetailSceen(),
     );
   }
